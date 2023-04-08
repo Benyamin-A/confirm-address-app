692d2cd3604994b751900303e9972c798ef6a23d
Ben <basgharz@uwo.ca>
2023-04-08 15:03:33 -0400

    add 2 components:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddressInputComponent } from './address-input/address-input.component';
+import { AddressConfirmationComponent } from './address-confirmation/address-confirmation.component';
+import { RouterModule, Routes,  } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'address-input', component: AddressInputComponent },
+  { path: 'address-confirmation', component: AddressConfirmationComponent },
+  { path: '', redirectTo: '/address-input', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddressInputComponent,
+    AddressConfirmationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
